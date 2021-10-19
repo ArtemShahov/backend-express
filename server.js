85402f168f71b7de1e5905e205274d4a0f133cd8
ArtemShahov <followdmdv@gmail.com>
2021-10-19 06:57:47 +0300

    mongodb

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const cors = require('cors');
 const app = express();
 const port = 8080;
 
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/test');
+
+const User = mongoose.model('User', { name: String,age: Number });
+
+const artem = new User({ name: 'Artem', age: 25 });
+artem.save().then(() => console.log('Success'));
+
 const data = 'hello world';
 
 app.use(cors());
@@ -13,6 +22,9 @@ app.get('/', (req, res) => {
     res.send(data);
 })
 
+app.get('/name', (req, res) => {
+  res.send('');
+})
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
